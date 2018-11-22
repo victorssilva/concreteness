a9186758754b9a10bbeaf596dffffca5b50218b9
Victor Silva <victsou@gmail.com>
2018-11-22 12:30:14 -0800

    break down concreteness file into two

--- concreteness.py
+++ concreteness.py
@@ -1,9 +1,7 @@
-import argparse
 import logging
 import math
 import os.path
 
-import mirflickr
 import numpy as np
 import torch
 import torchvision.models as models
@@ -12,7 +10,9 @@ import torchvision.transforms as transforms
 from annoy import AnnoyIndex
 from PIL import Image
 from torch.autograd import Variable
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
+
+import mirflickr
 
 cuda = torch.cuda.is_available()
 
@@ -140,7 +140,7 @@ def get_concreteness_for_word(word, associated_images, filtered_image_to_index, 
   return mni / denominator
 
 
-def get_concreteness(dataset, annoy_index_file, k=50):
+def get_concreteness(dataset, annoy_index_file, k):
   images_by_tag = get_images_by_tag(dataset.filtered_tags)
   filtered_image_to_index = dataset.get_filtered_image_to_index()
   n = len(dataset.filtered_tags)
@@ -210,63 +210,3 @@ def build_nns(img_vectors, k, annoy_index_file=None):
       log.debug("Done saving NNS for %s out of %s", index, len(img_vectors))
 
   return nns
-
-
-def _setup_logging(verbose):
-  logging_level = logging.DEBUG if verbose else logging.INFO
-  logging_format='%(asctime)s [%(levelname)s] %(message)s'
-  logging.basicConfig(level=logging_level, format=logging_format)
-
-
-def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("-d", "--dataset-dir", type=str, required=True,
-                      help="Path to the directory of the mirflickr dataset.")
-  parser.add_argument("-c", "--cache-dir", type=str, required=False,
-                      help="Path to a directory to use for cache.")
-  parser.add_argument("-v", "--verbose", help="Increase output verbosity.",
-                      action="store_true")
-  args = parser.parse_args()
-
-  _setup_logging(args.verbose)
-
-  images_directory = os.path.join(args.dataset_dir, "images")
-  tags_directory = os.path.join(args.dataset_dir, "tags")
-
-  vectors_file = None
-  annoy_index_file = None
-  if args.cache_dir is not None:
-    vectors_file = os.path.join(args.cache_dir, "vectors.pt")
-    annoy_index_file = os.path.join(args.cache_dir, "index.ann")
-
-  log.info("Loading dataset.")
-  dataset = mirflickr.MirflickrImagesDataset(images_directory, tags_directory,
-                                             transform=get_tensor_for_image)
-  log.info("Dataset is loaded.")
-
-  if vectors_file is not None and os.path.isfile(vectors_file):
-    img_vectors = torch.load(vectors_file)
-  else:
-    log.info("Building image vectors.")
-    img_vectors = build_image_vectors(dataset)
-    log.info("Built image vectors.")
-
-    if vectors_file is not None:
-      torch.save(img_vectors, vectors_file)
-      log.info("Saved image vectors to %s", vectors_file)
-
-  log.info("Computing concreteness.")
-  k = 50
-  nns = build_nns(img_vectors, k, annoy_index_file=annoy_index_file)
-  concreteness = get_concreteness_precomputed_nns(dataset, nns, k)
-  log.info("Done!")
-
-  sorted_concreteness = sorted(concreteness.items(), key=lambda x: x[1], reverse=True)
-  len(sorted_concreteness)
-
-  from IPython import embed
-  embed()
-
-
-if __name__ == "__main__":
-  main()
---
+++ main.py
@@ -0,0 +1,73 @@
+import argparse
+import logging
+import os.path
+
+import torch
+
+import concreteness
+import mirflickr
+
+log = logging.getLogger(__name__)
+
+DEFAULT_K = 50
+
+
+def _setup_logging(verbose):
+  logging_level = logging.DEBUG if verbose else logging.INFO
+  logging_format='%(asctime)s [%(levelname)s] %(message)s'
+  logging.basicConfig(level=logging_level, format=logging_format)
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-d", "--dataset-dir", type=str, required=True,
+                      help="Path to the directory of the mirflickr dataset.")
+  parser.add_argument("-c", "--cache-dir", type=str, required=False,
+                      help="Path to a directory to use for cache.")
+  parser.add_argument("-v", "--verbose", help="Increase output verbosity.",
+                      action="store_true")
+  parser.add_argument("-k", help="Number of neighbos to search for.", type=int,
+                      default=DEFAULT_K, required=False)
+  args = parser.parse_args()
+
+  _setup_logging(args.verbose)
+
+  images_directory = os.path.join(args.dataset_dir, "images")
+  tags_directory = os.path.join(args.dataset_dir, "tags")
+
+  vectors_file = None
+  annoy_index_file = None
+  if args.cache_dir is not None:
+    vectors_file = os.path.join(args.cache_dir, "vectors.pt")
+    annoy_index_file = os.path.join(args.cache_dir, "index.ann")
+
+  log.info("Loading dataset.")
+  dataset = mirflickr.MirflickrImagesDataset(images_directory, tags_directory,
+                                             transform=concreteness.get_tensor_for_image)
+  log.info("Dataset is loaded.")
+
+  if vectors_file is not None and os.path.isfile(vectors_file):
+    img_vectors = torch.load(vectors_file)
+  else:
+    log.info("Building image vectors.")
+    img_vectors = concreteness.build_image_vectors(dataset)
+    log.info("Built image vectors.")
+
+    if vectors_file is not None:
+      torch.save(img_vectors, vectors_file)
+      log.info("Saved image vectors to %s", vectors_file)
+
+  log.info("Computing concreteness.")
+  nns = concreteness.build_nns(img_vectors, args.k, annoy_index_file=annoy_index_file)
+  concreteness_dict = concreteness.get_concreteness_precomputed_nns(dataset, nns, args.k)
+  log.info("Done!")
+
+  sorted_concreteness = sorted(concreteness_dict.items(), key=lambda x: x[1], reverse=True)
+  len(sorted_concreteness)
+
+  from IPython import embed
+  embed()
+
+
+if __name__ == "__main__":
+  main()
