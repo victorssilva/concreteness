644c92e8d7fafa74157a29345e54c980b27cc7d7
Victor Silva <victsou@gmail.com>
2018-11-22 14:05:58 -0800

    Use 4 spaces for indentation

--- concreteness.py
+++ concreteness.py
@@ -20,8 +20,8 @@ log = logging.getLogger(__name__)
 
 
 def show_image(dataset, image_index):
-  image = dataset.get_pil_image(image_index)
-  imshow(np.asarray(image))
+    image = dataset.get_pil_image(image_index)
+    imshow(np.asarray(image))
 
 
 # These are the expected values of the following pre-trained model.
@@ -32,18 +32,18 @@ expected_std = [0.229, 0.224, 0.225]
 
 
 def get_resnet_model():
-  resnet = models.resnet50(pretrained=True)
+    resnet = models.resnet50(pretrained=True)
 
-  for param in resnet.parameters():
-    param.requires_grad = False
+    for param in resnet.parameters():
+        param.requires_grad = False
 
-  resnet.eval()
+    resnet.eval()
 
-  if cuda:
-    device = torch.device("cuda")
-    resnet.to(device)
+    if cuda:
+        device = torch.device("cuda")
+        resnet.to(device)
 
-  return resnet
+    return resnet
 
 
 scaler = transforms.Resize(input_image_size)
@@ -52,161 +52,161 @@ to_tensor = transforms.ToTensor()
 
 
 def img2vec(resnet, image_tensors):
-  last_layer = resnet._modules.get("avgpool")
+    last_layer = resnet._modules.get("avgpool")
 
-  if cuda:
-      image_tensors = image_tensors.cuda()
+    if cuda:
+        image_tensors = image_tensors.cuda()
 
-  embedding = torch.zeros(image_tensors.shape[0], 2048, 1, 1)
+    embedding = torch.zeros(image_tensors.shape[0], 2048, 1, 1)
 
-  def copy_output(m, i, o):
-      embedding.copy_(o.data)
+    def copy_output(m, i, o):
+        embedding.copy_(o.data)
 
-  h = last_layer.register_forward_hook(copy_output)
-  resnet(image_tensors)
-  h.remove()
+    h = last_layer.register_forward_hook(copy_output)
+    resnet(image_tensors)
+    h.remove()
 
-  return embedding
+    return embedding
 
 
 def get_tensor_for_image(image):
-  variable = Variable(normalize(to_tensor(scaler(image))))
-  return variable
+    variable = Variable(normalize(to_tensor(scaler(image))))
+    return variable
 
 
 def build_image_vectors(dataset, batch_size=64, num_workers=4):
-  resnet = get_resnet_model()
-  dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
+    resnet = get_resnet_model()
+    dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
 
-  img_vectors = torch.zeros(len(dataset), 2048, 1, 1)
-  number_of_batches = math.ceil(len(dataset) / batch_size)
+    img_vectors = torch.zeros(len(dataset), 2048, 1, 1)
+    number_of_batches = math.ceil(len(dataset) / batch_size)
 
-  for batch_index, batch in enumerate(dataloader):
-    batch_start = batch_index * batch_size
-    batch_end = batch_start + batch_size
+    for batch_index, batch in enumerate(dataloader):
+        batch_start = batch_index * batch_size
+        batch_end = batch_start + batch_size
 
-    # Pad the last batch if it's smaller than batch_size
-    if batch.shape[0] != batch_size:
-      new_batch = torch.zeros(batch_size, batch.shape[1], batch.shape[2],
-                              batch.shape[3])
-      new_batch[:batch.shape[0]] = batch
-      img_vectors[batch_start:batch_end] = img2vec(resnet, new_batch)[:batch.shape[0]]
-      continue
+        # Pad the last batch if it's smaller than batch_size
+        if batch.shape[0] != batch_size:
+            new_batch = torch.zeros(batch_size, batch.shape[1], batch.shape[2],
+                                    batch.shape[3])
+            new_batch[:batch.shape[0]] = batch
+            img_vectors[batch_start:batch_end] = img2vec(resnet, new_batch)[:batch.shape[0]]
+            continue
 
-    img_vectors[batch_start:batch_end] = img2vec(resnet, batch)
-    log.debug("Computed batch %s out of %s", batch_index, number_of_batches)
+        img_vectors[batch_start:batch_end] = img2vec(resnet, batch)
+        log.debug("Computed batch %s out of %s", batch_index, number_of_batches)
 
-  return img_vectors
+    return img_vectors
 
 
 def get_images_by_tag(filtered_tags):
-  images_by_tag = {}
-  for image, tags in filtered_tags.items():
-    for tag in tags:
-      images_by_tag.setdefault(tag, set()).add(image)
+    images_by_tag = {}
+    for image, tags in filtered_tags.items():
+        for tag in tags:
+            images_by_tag.setdefault(tag, set()).add(image)
 
-  return images_by_tag
+    return images_by_tag
 
 
 def build_annoy_index(img_vectors):
-  annoy_index = AnnoyIndex(2048)
-  for i in range(len(img_vectors)):
-    annoy_index.add_item(i, img_vectors[i])
+    annoy_index = AnnoyIndex(2048)
+    for i in range(len(img_vectors)):
+        annoy_index.add_item(i, img_vectors[i])
 
-  annoy_index.build(10)
-  return annoy_index
+    annoy_index.build(10)
+    return annoy_index
 
 
 def load_annoy_index(file):
-  annoy_index = AnnoyIndex(2048)
-  annoy_index.load(file)
-  return annoy_index
+    annoy_index = AnnoyIndex(2048)
+    annoy_index.load(file)
+    return annoy_index
 
 
 def get_concreteness_for_word(word, associated_images, filtered_image_to_index, annoy_index_file,
                               n, k):
-  annoy_index = load_annoy_index(annoy_index_file)
-  mni = 0.0
+    annoy_index = load_annoy_index(annoy_index_file)
+    mni = 0.0
 
-  for image in associated_images:
-    if image not in filtered_image_to_index:
-      continue
+    for image in associated_images:
+        if image not in filtered_image_to_index:
+            continue
 
-    neighbors = set(annoy_index.get_nns_by_item(filtered_image_to_index[image], k))
-    mni += 1.0 * (len(associated_images.intersection(neighbors)))
+        neighbors = set(annoy_index.get_nns_by_item(filtered_image_to_index[image], k))
+        mni += 1.0 * (len(associated_images.intersection(neighbors)))
 
-  mni = mni / len(associated_images)
-  denominator = (1.0 * k * len(associated_images)) / n
-  return mni / denominator
+    mni = mni / len(associated_images)
+    denominator = (1.0 * k * len(associated_images)) / n
+    return mni / denominator
 
 
 def get_concreteness(dataset, annoy_index_file, k):
-  images_by_tag = get_images_by_tag(dataset.filtered_tags)
-  filtered_image_to_index = dataset.get_filtered_image_to_index()
-  n = len(dataset.filtered_tags)
+    images_by_tag = get_images_by_tag(dataset.filtered_tags)
+    filtered_image_to_index = dataset.get_filtered_image_to_index()
+    n = len(dataset.filtered_tags)
 
-  with mp.Pool() as pool:
-    results = {}
-    for word in images_by_tag:
-      associated_images = images_by_tag[word]
-      results[word] = pool.apply_async(get_concreteness_for_word,
-                                       (word, associated_images, filtered_image_to_index,
-                                        annoy_index_file, n, k))
+    with mp.Pool() as pool:
+        results = {}
+        for word in images_by_tag:
+            associated_images = images_by_tag[word]
+            results[word] = pool.apply_async(get_concreteness_for_word,
+                                             (word, associated_images, filtered_image_to_index,
+                                              annoy_index_file, n, k))
 
-  concreteness = {word: result.get() for (word, result) in results.items()}
-  return concreteness
+    concreteness = {word: result.get() for (word, result) in results.items()}
+    return concreteness
 
 
 def get_concreteness_for_word_precomputed_nns(word, associated_images, filtered_image_to_index,
                                               nns, n, k):
-  mni = 0.0
+    mni = 0.0
 
-  for image in associated_images:
-    if image not in filtered_image_to_index:
-      continue
+    for image in associated_images:
+        if image not in filtered_image_to_index:
+            continue
 
-    neighbors = nns[filtered_image_to_index[image]]
-    mni += 1.0 * (len(associated_images.intersection(neighbors)))
+        neighbors = nns[filtered_image_to_index[image]]
+        mni += 1.0 * (len(associated_images.intersection(neighbors)))
 
-  mni = mni / len(associated_images)
-  denominator = (1.0 * k * len(associated_images)) / n
-  return mni / denominator
+    mni = mni / len(associated_images)
+    denominator = (1.0 * k * len(associated_images)) / n
+    return mni / denominator
 
 
 def get_concreteness_precomputed_nns(dataset, nns, k):
-  images_by_tag = get_images_by_tag(dataset.filtered_tags)
-  filtered_image_to_index = dataset.get_filtered_image_to_index()
-  n = len(dataset.filtered_tags)
+    images_by_tag = get_images_by_tag(dataset.filtered_tags)
+    filtered_image_to_index = dataset.get_filtered_image_to_index()
+    n = len(dataset.filtered_tags)
 
-  concreteness = {}
-  i = 0
-  for word in images_by_tag:
-    associated_images = images_by_tag[word]
-    concreteness[word] = get_concreteness_for_word_precomputed_nns(
-      word, associated_images, filtered_image_to_index, nns, n, k)
-    if not i % 1000:
-      log.debug("Done with word %s out of %s", i, len(images_by_tag))
-    i += 1
-  return concreteness
+    concreteness = {}
+    i = 0
+    for word in images_by_tag:
+        associated_images = images_by_tag[word]
+        concreteness[word] = get_concreteness_for_word_precomputed_nns(
+            word, associated_images, filtered_image_to_index, nns, n, k)
+        if not i % 1000:
+            log.debug("Done with word %s out of %s", i, len(images_by_tag))
+        i += 1
+    return concreteness
 
 
 def build_nns(img_vectors, k, annoy_index_file=None):
-  if annoy_index_file is not None and os.path.isfile(annoy_index_file):
-    annoy_index = load_annoy_index(annoy_index_file)
-    log.info("Loaded annoy index.")
-  else:
-    log.info("Building annoy index.")
-    annoy_index = build_annoy_index(img_vectors)
+    if annoy_index_file is not None and os.path.isfile(annoy_index_file):
+        annoy_index = load_annoy_index(annoy_index_file)
+        log.info("Loaded annoy index.")
+    else:
+        log.info("Building annoy index.")
+        annoy_index = build_annoy_index(img_vectors)
 
-    if annoy_index_file is not None:
-      annoy_index.save(annoy_index_file)
-      log.info("Annoy index was saved to %s.", annoy_index_file)
+        if annoy_index_file is not None:
+            annoy_index.save(annoy_index_file)
+            log.info("Annoy index was saved to %s.", annoy_index_file)
 
-  log.info("Bulding NNS map.")
-  nns = {}
-  for index in range(len(img_vectors)):
-    nns[index] = set(annoy_index.get_nns_by_item(index, k))
-    if not index % 1000:
-      log.debug("Done saving NNS for %s out of %s", index, len(img_vectors))
+    log.info("Bulding NNS map.")
+    nns = {}
+    for index in range(len(img_vectors)):
+        nns[index] = set(annoy_index.get_nns_by_item(index, k))
+        if not index % 1000:
+            log.debug("Done saving NNS for %s out of %s", index, len(img_vectors))
 
-  return nns
+    return nns
--- mirflickr.py
+++ mirflickr.py
@@ -10,119 +10,119 @@ log = logging.getLogger(__name__)
 
 
 def get_tags(tags_file):
-  with open(tags_file, encoding="utf8") as f:
-    return list(map(lambda x: x.strip(), f.readlines()))
+    with open(tags_file, encoding="utf8") as f:
+        return list(map(lambda x: x.strip(), f.readlines()))
 
 
 def get_index_from_tags_file(tags_file):
-  return int(tags_file.split("/")[-1][:-4])
+    return int(tags_file.split("/")[-1][:-4])
 
 
 def build_tags_json(tag_files, tags_json_filename):
-  image_tags = {}
+    image_tags = {}
 
-  for i, tags_file in enumerate(tag_files):
-    image_tags[get_index_from_tags_file(tags_file)] = get_tags(tags_file)
+    for i, tags_file in enumerate(tag_files):
+        image_tags[get_index_from_tags_file(tags_file)] = get_tags(tags_file)
 
-  with open(tags_json_filename, "w", encoding="utf8") as tags_json_file:
-    json.dump(image_tags, tags_json_file)
+    with open(tags_json_filename, "w", encoding="utf8") as tags_json_file:
+        json.dump(image_tags, tags_json_file)
 
-  return image_tags
-
-
-def load_tags_json(tags_json_filename):
-  with open(tags_json_filename, "r") as tags_json_file:
-    image_tags = json.load(tags_json_file)
     return image_tags
 
 
-def get_tag_scores(image_tags):
-  tag_scores = {}
-  for tags in image_tags.values():
-    for tag in tags:
-      tag = tag.lower()
-      tag_scores[tag] = tag_scores.get(tag, 0) + 1
+def load_tags_json(tags_json_filename):
+    with open(tags_json_filename, "r") as tags_json_file:
+        image_tags = json.load(tags_json_file)
+        return image_tags
 
-  return tag_scores
+
+def get_tag_scores(image_tags):
+    tag_scores = {}
+    for tags in image_tags.values():
+        for tag in tags:
+            tag = tag.lower()
+            tag_scores[tag] = tag_scores.get(tag, 0) + 1
+
+    return tag_scores
 
 
 def get_filtered_tags(image_tags):
-  tag_scores = get_tag_scores(image_tags)
+    tag_scores = get_tag_scores(image_tags)
 
-  # Will keep only the tags that have at least 200 occurrences
-  filtered_tags = {}
-  for image, tags in image_tags.items():
-    filtered_image_tags = []
-    for tag in tags:
-      if tag_scores.get(tag, 0) >= 200:
-        filtered_image_tags.append(tag)
+    # Will keep only the tags that have at least 200 occurrences
+    filtered_tags = {}
+    for image, tags in image_tags.items():
+        filtered_image_tags = []
+        for tag in tags:
+            if tag_scores.get(tag, 0) >= 200:
+                filtered_image_tags.append(tag)
 
-    if filtered_image_tags:
-      filtered_tags[int(image)] = filtered_image_tags
+        if filtered_image_tags:
+            filtered_tags[int(image)] = filtered_image_tags
 
-  return filtered_tags
+    return filtered_tags
 
 
 def list_tag_files(tags_directory):
-  tag_files = []
-  for subdir in os.listdir(tags_directory):
-    subdir_files = os.listdir(os.path.join(tags_directory, subdir))
-    for file in subdir_files:
-      tag_files.append(os.path.join(tags_directory, subdir, file))
+    tag_files = []
+    for subdir in os.listdir(tags_directory):
+        subdir_files = os.listdir(os.path.join(tags_directory, subdir))
+        for file in subdir_files:
+            tag_files.append(os.path.join(tags_directory, subdir, file))
 
-  return sorted(tag_files, key=lambda f: get_index_from_tags_file(f))
+    return sorted(tag_files, key=lambda f: get_index_from_tags_file(f))
 
 
 def get_image_score(image_tags, image_index):
-  return len(image_tags.get(image_index, []))
+    return len(image_tags.get(image_index, []))
 
 
 def get_filtered_images(filtered_tags, image_indexes, min_score=3):
-  return [img for img in image_indexes if get_image_score(filtered_tags, img) >= min_score]
+    return [img for img in image_indexes if get_image_score(filtered_tags, img) >= min_score]
 
 
 class MirflickrImagesDataset(Dataset):
-  dataset_size = 1000000
-  subdirectory_size = 10000
+    dataset_size = 1000000
+    subdirectory_size = 10000
 
-  def __init__(self, images_directory, tags_directory, transform=None):
-    self.images_directory = images_directory
-    self.transform = transform
+    def __init__(self, images_directory, tags_directory, transform=None):
+        self.images_directory = images_directory
+        self.transform = transform
 
-    image_indexes = list(range(self.dataset_size))
+        image_indexes = list(range(self.dataset_size))
 
-    tags_json_filename = os.path.join(tags_directory, "tags.json")
-    try:
-      image_tags = load_tags_json(tags_json_filename)
-    except FileNotFoundError:
-      image_tags = build_tags_json(list_tag_files(tags_directory), tags_json_filename)
+        tags_json_filename = os.path.join(tags_directory, "tags.json")
+        try:
+            image_tags = load_tags_json(tags_json_filename)
+        except FileNotFoundError:
+            image_tags = build_tags_json(list_tag_files(tags_directory), tags_json_filename)
 
-    self.filtered_tags = get_filtered_tags(image_tags)
-    self.filtered_images = get_filtered_images(self.filtered_tags, image_indexes)
+        self.filtered_tags = get_filtered_tags(image_tags)
+        self.filtered_images = get_filtered_images(self.filtered_tags, image_indexes)
 
-  def __len__(self):
-    return len(self.filtered_images)
+    def __len__(self):
+        return len(self.filtered_images)
 
-  def get_image_path(self, image_index):
-    image_subdirectory = str(int(image_index / self.subdirectory_size))
-    return os.path.join(self.images_directory, image_subdirectory, "{}.jpg".format(image_index))
+    def get_image_path(self, image_index):
+        image_subdirectory = str(int(image_index / self.subdirectory_size))
+        return os.path.join(self.images_directory, image_subdirectory, "{}.jpg".format(image_index))
 
-  def get_pil_image(self, index):
-    return Image.open(self.get_image_path(self.filtered_images[index]))
+    def get_pil_image(self, index):
+        return Image.open(self.get_image_path(self.filtered_images[index]))
 
-  def __getitem__(self, index):
-    image = self.get_pil_image(index)
-    if self.transform:
-      try:
-        image = self.transform(image)
-      except Exception as e:
-        log.warning("Could not transform image %s due to %s. Skipping.", index, e)
-        return torch.zeros(3, 224, 224)
+    def __getitem__(self, index):
+        image = self.get_pil_image(index)
+        if self.transform:
+            try:
+                image = self.transform(image)
+            except Exception as e:
+                log.warning("Could not transform image %s due to %s. Skipping.", index, e)
+                return torch.zeros(3, 224, 224)
 
-    return image
+        return image
 
-  def get_filtered_image_to_index(self):
-    filtered_image_to_index = {}
-    for i, image_index in enumerate(self.filtered_images):
-      filtered_image_to_index[image_index] = i
-    return filtered_image_to_index
+    def get_filtered_image_to_index(self):
+        filtered_image_to_index = {}
+        for i, image_index in enumerate(self.filtered_images):
+            filtered_image_to_index[image_index] = i
+        return filtered_image_to_index
